5553a04cab7263823459e357a4b283424f2366d2
Jocelyn Q <jocelyn.develop@gmail.com>
2023-09-20 20:24:10 +0200

    [ADD] first question

--- expense.py
+++ expense.py
@@ -1,30 +1,44 @@
+import csv
 from PyInquirer import prompt
 
 expense_questions = [
     {
-        "type":"input",
-        "name":"amount",
-        "message":"New Expense - Amount: ",
+        "type": "input",
+        "name": "amount",
+        "message": "New Expense - Amount: ",
     },
     {
-        "type":"input",
-        "name":"label",
-        "message":"New Expense - Label: ",
+        "type": "input",
+        "name": "label",
+        "message": "New Expense - Label: ",
     },
     {
-        "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
+        "type": "input",
+        "name": "spender",
+        "message": "New Expense - Spender: ",
     },
-
 ]
 
+EXPENSES_FILE = "expense_report.csv"
 
 
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    expense = {
+        "amount": infos["amount"],
+        "label": infos["label"],
+        "spender": infos["spender"],
+    }
+    save_to_csv_expense(expense)
     print("Expense Added !")
     return True
 
 
+def save_to_csv_expense(expense):
+    with open(EXPENSES_FILE, "a", newline="") as csvfile:
+        fieldnames = ["amount", "label", "spender"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Écrivez les données de la dépense dans le fichier CSV
+        writer.writerow(expense)
